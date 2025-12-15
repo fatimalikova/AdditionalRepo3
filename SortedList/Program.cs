5db32cb5bc1c86927f10fa5ec103d598fba20a0b
fatimalikova <fatimakhm@code.edu.az>
2025-12-15 19:26:52 +0400

    Stack Practice (Generic and Non-Generic)

--- SortedList/Program.cs
+++ SortedList/Program.cs
@@ -28,21 +28,57 @@ namespace SortedListApp
 
             #region SortedList(Non-Generic)
 
-            SortedList sortedList = new SortedList();
-            sortedList.Add(3, "Three");
-            sortedList.Add(1, "One");
-            sortedList.Add(2, "Two");
-            sortedList.Add(5, "Five");
-            sortedList.Add(4, "Four");
-            sortedList.Remove(2);
-            sortedList.Capacity = 10;
-            sortedList.TrimToSize();
-            Console.WriteLine(sortedList.ContainsKey(7));
-            Console.WriteLine("====================================");
-            foreach (DictionaryEntry item in sortedList)
-            {
-                Console.WriteLine($"{item.Key}: {item.Value}");
-            }
+            //SortedList sortedList = new SortedList();
+            //sortedList.Add(3, "Three");
+            //sortedList.Add(1, "One");
+            //sortedList.Add(2, "Two");
+            //sortedList.Add(5, "Five");
+            //sortedList.Add(4, "Four");
+            //sortedList.Remove(2);
+            //sortedList.Capacity = 10;
+            //sortedList.TrimToSize();
+            //Console.WriteLine(sortedList.ContainsKey(7));
+            //Console.WriteLine("====================================");
+            //foreach (DictionaryEntry item in sortedList)
+            //{
+            //    Console.WriteLine($"{item.Key}: {item.Value}");
+            //}
+            #endregion
+
+            #region Stack(Generic) LIFO
+            //Stack<int> stack = new Stack<int>();
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+            //Console.WriteLine("Stack contents:");
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine($"Popped item: {stack.Pop()}");
+            //Console.WriteLine("Stack contents after pop:");
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Stack(Non-Generic) LIFO
+            //Stack stack = new Stack(8);
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+            //Console.WriteLine("Stack contents:");
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine($"Popped item: {stack.Pop()}");
+            //Console.WriteLine("Stack contents after pop:");
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
             #endregion
         }
     }
