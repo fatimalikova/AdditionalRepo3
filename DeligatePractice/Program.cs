c4a8c46fbd851d7893a74f577ff5fdc33b664d16
fatimalikova <fatimakhm@code.edu.az>
2025-12-29 20:59:05 +0400

    Added

--- DeligatePractice/Program.cs
+++ DeligatePractice/Program.cs
@@ -8,30 +8,30 @@
         public delegate U GenericMethod<in T,out U>(T parameter);
         static void Main(string[] args)
         {
-            //Sum((s) => s % 2 == 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-            //Sum(delegate (int s) 
-            //{ return s % 2 != 0;}, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            Sum((s) => s % 2 == 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            Sum(delegate (int s)
+            { return s % 2 != 0; }, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-            //NumberMethod method = IsEven;
-            //method += IsOdd;
-            ////method(1);
-            ////method.Invoke(2);
+            NumberMethod method = IsEven;
+            method += IsOdd;
+            //method(1);
+            //method.Invoke(2);
 
-            //method += delegate (int number)
-            //{
-            //    return number > 5;
-            //};
+            method += delegate (int number)
+            {
+                return number > 5;
+            };
 
-            //method += (s) => s < 5;
-            //method(2);
+            method += (s) => s < 5;
+            method(2);
 
-            //ResultMethod<int> resultMethod = delegate (int number)
-            //{
-            //    return number * number;
-            //};
-            //ResultMethod<string> resultMethod1 = (s) => s + " Hello";
-            //Console.WriteLine(resultMethod1("Test"));
-            //Console.WriteLine(resultMethod(3));
+            ResultMethod<int> resultMethod = delegate (int number)
+            {
+                return number * number;
+            };
+            ResultMethod<string> resultMethod1 = (s) => s + " Hello";
+            Console.WriteLine(resultMethod1("Test"));
+            Console.WriteLine(resultMethod(3));
 
             GenericMethod<int, string> genericMethod = (s) => s.ToString() + " Generic Method";
             Console.WriteLine(genericMethod(5));
@@ -94,14 +94,14 @@
         }
 
         ////////////////////////////////////////////////////
-        //public static bool IsEven(int number)
-        //{
-        //    return number % 2 == 0;
-        //}
-        //public static bool IsOdd(int number)
-        //{
-        //    return number % 2 != 0;
-        //}
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
         ///////////////////////////////////////////////////
     }
 }
